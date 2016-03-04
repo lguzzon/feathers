8cc448697f8dc790e9fb68b11030de167b6014ad
Eric Kryski <e.kryski@gmail.com>
2016-03-04 13:44:15 -0700

    minifying the builds when in production

--- gulp/tasks/webpack.js
+++ gulp/tasks/webpack.js
@@ -1,10 +1,17 @@
 var gulp = require('gulp');
+var util = require('gulp-util');
+var webpack = require('webpack');
+var webpackStream = require('webpack-stream');
 var config = require('../config').webpack;
-var webpack = require('webpack-stream');
+var production = !!util.env.production || process.env.NODE_ENV === 'production';
+
+if (production) {
+  config.plugins.push(new webpack.optimize.UglifyJsPlugin());
+}
 
 gulp.task('webpack', function () {
   gulp.src(config.src)
-    .pipe(webpack(config), function(err, stats) {
+    .pipe(webpackStream(config), function(err, stats) {
       if (err) {
         throw new gutil.PluginError('[webpack]', err);
       }
