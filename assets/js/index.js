d2ef56720500744fb78de6adf95405fd27e62623
Eric Kryski <e.kryski@gmail.com>
2016-03-15 01:24:50 -0600

    adding product hunt banner

--- assets/js/index.js
+++ assets/js/index.js
@@ -9,11 +9,28 @@ jQuery.fn.reverse = [].reverse;
 
 var App = function() {
   $('document').ready(function(){
+
+    function getParameterByName(name, url) {
+        if (!url) url = window.location.href;
+        url = url.toLowerCase(); // This is just to avoid case sensitiveness  
+        name = name.replace(/[\[\]]/g, "\\$&").toLowerCase();// This is just to avoid case sensitiveness for query parameter name
+        var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+            results = regex.exec(url);
+        if (!results) return null;
+        if (!results[2]) return '';
+        return decodeURIComponent(results[2].replace(/\+/g, " "));
+    }
+
     var deviceAnimationInterval;
     var $tagline = $('h2.tagline');
     var $mainCTA = $('.hero .button-primary');
     var $navLinks = $('.navbar ul a');
     typewriter.prepare('.typewriter');
+
+    if (getParameterByName('ref') === 'producthunt') {
+      // TODO: Show product hunt banner
+      $('.product-hunt').velocity('transition.slideDownIn');
+    }
 
     var animateDevices = function() {
       var $user1 = $('svg .user-1');
